28ddc5edde9e18da95f1219dc06dc3169654a9d9
L0rence <lawrence.chandru@gmail.com>
2021-04-12 13:36:42 +1200

    kkj

--- SeleniumBasic/EmptyClass.cs
+++ SeleniumBasic/EmptyClass.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 namespace SeleniumBasic
 {
     public class EmptyClass
     {
         public EmptyClass()
         {
-            Console.WriteLine("latest !");
+            IWebDriver driver  = new ChromeDriver();
+            driver.Url = "https://www.google.com/ncr";
+            driver.FindElement(By.Name("q")).SendKeys("cheese" + Keys.Enter);
+
+
+           // WebDriverWait wait = new(driver, TimeSpan.FromSeconds(20));
+
+
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+
+            IWebElement firstResult = wait.Until(e => e.FindElement(By.XPath("//a/h3")));
+
+            Console.WriteLine(firstResult.Text);
         }
     }
 }
